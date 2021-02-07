ffd8d9571d00cc71acd5388eb8b615870eedfc8c
Garvit Chouhan <68856476+Garvit9000c@users.noreply.github.com>
2021-02-07 14:19:11 +0530

    Update Code.c

--- Project_1/Code.c
+++ Project_1/Code.c
@@ -1,2 +1,6 @@
-/*Read a Poem from File.txt ,& find its goodness Value = |U - L|  here,U=no of upper case char L=no of palindrome String  */
+/*Read a Sentence from File.txt ,& state Weather its good or not?, "a good sentence must be palindrome & difference between total char and Upper case char>0  */
+/*Task1 read file.txt store its input in string using (fgets function)*/  
+/*Task2 check weather str is palindrom if yes store flag=True else flag=false(strrev funtion*/
+/*Task3 find x=(Number of upper case char)-(total char) , in str(isuppper funtion)*/
+/*Task4 find mod(x), if its >0 and flag==true, print its a good sentence otherwise bad sentence(fabs function)*/
 /*Code Below This*/
