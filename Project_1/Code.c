9995179f5890ad5fd856e153ac0e8a8b31c9779f
Garvit Chouhan <68856476+Garvit9000c@users.noreply.github.com>
2021-02-07 19:05:06 +0530

    Update Code.c

--- Project_1/Code.c
+++ Project_1/Code.c
@@ -1,26 +1,11 @@
 /*Read a Sentence from File.txt ,& state Weather its good or not?, "a good sentence must be palindrome & difference between total char and Upper case char>0  */
 /*Task1 read file.txt store its input in string using (fgets function)*/  
-/*Task2 check weather str is palindrom if yes store flag=True else flag=false(strrev funtion*/
+/*Task2 check weather str is palindrom if yes store flag=1 else flag=2 (strrev funtion)*/
 /*Task3 find x=(Number of upper case char)-(total char) , in str(isuppper funtion)*/
 /*Task4 find mod(x), if its >0 and flag==true, print its a good sentence otherwise bad sentence(fabs function)*/
 /*Code Below This*/
 #include <stdio.h>
-int main()
-{
-    //Task1 start
-    FILE *fp;
-    char str[60];
-    fp=fopen("file.txt","r");
-    fgets(str,60,fp);
-    fclose(fp);
-    //Task1 end 
-    
-    
-    return(0);
-}
-// Task 2 start
 #include <string.h>
-
 char *strrev(char *str)
 {
       char *p1, *p2;
@@ -38,14 +23,25 @@ char *strrev(char *str)
 
 int main()
 {
-    char str[50] = "madam";
-    char temp[50];
-    strcpy(temp, str); // temp=s
+    //Task1 start
+    FILE *fp;
+    char str[60];
+    fp=fopen("file.txt","r");
+    fgets(str,60,fp);
+    fclose(fp);
+    
+    //Task2
+    char temp[60];
+    strcpy(temp,str); //temp=s
+    int flag;
     strrev(str);
-    if(strcmp(str, temp) == 0)
-printf("The entered string is a palindrome.");
-else
-printf("The entered string is not a palindrome.");
+    if(strcmp(str, temp) == 0){
+        flag=1;
+    }
+    else{
+        flag=0;
+    }
+    return(0);
 }
-//Task2 end
-Return 0;
+
+
